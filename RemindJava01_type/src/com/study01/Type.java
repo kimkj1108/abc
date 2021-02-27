29b4ce95de411dac004490476596c10d3762da1f
kimkj1108 <kimkj1108@gmail.com>
2021-02-27 22:53:17 +0900

    Start Java study

---
+++ RemindJava01_type/src/com/study01/Type.java
@@ -0,0 +1,16 @@
+package com.study01;
+
+public class Type {
+	public static void main(String[] args) {
+		//정수형 type : byte(1), short(2), int(4), long(8)
+		//타입 변수 = 값;
+		byte a01 = 1;
+		System.out.println(a01);
+		a01 = 2;
+		System.out.println(a01);
+		
+		byte a02 = 127;
+		System.out.println(a02);
+		
+	}
+}
